2f3a46819c8d513c59c790c05c2c0f2f10c77740
dcunha <daniel.stock@unico.io>
2024-01-05 14:12:01 -0300

    add more unit tests

--- github-actions-101/github-actions-101/UnitTest1.cs
+++ github-actions-101/github-actions-101/UnitTest1.cs
@@ -12,4 +12,16 @@ public class Tests
     {
         Assert.Pass();
     }
+
+    [Test]
+    public void Test2()
+    {
+        Assert.Pass();
+    }
+
+    [Test]
+    public void Test3()
+    {
+        Assert.Pass();
+    }
 }
