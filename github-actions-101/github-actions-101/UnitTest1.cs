ed1f66a5b9b08e35befda1de3a7c7967ed596866
dcunha <daniel.stock@unico.io>
2024-01-05 14:07:28 -0300

    initial commit

---
+++ github-actions-101/github-actions-101/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace github_actions_101;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
